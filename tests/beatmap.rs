fa05d611649dab91b427f7d4aa45000ca5fe42db
Ausakura <17104706+Ausakura@users.noreply.github.com>
2022-02-10 13:10:19 -0500

    second test file & debug

--- tests/beatmap.rs
+++ tests/beatmap.rs
@@ -1,16 +1,13 @@
 mod tests {
-    use std::{env};
-    use sekkei::parser::{
-        beatmap::{
-            BeatmapFile
-        }
-    };
+    use sekkei::parser::beatmap::BeatmapFile;
+    use std::env;
 
     #[test]
     fn test_parser() {
-        let path = env::current_dir().unwrap().to_str().unwrap().to_string() + "/tests/files/IMAGINARY LIKE THE JUSTICE.osu";
+        let path = env::current_dir().unwrap().to_str().unwrap().to_string()
+            + "/tests/files/kakushigoto.osu";
         let bm = BeatmapFile::from_file(&path);
-        
+
         // format ver
         assert_eq!(bm.format_version, 14);
 
@@ -18,5 +15,8 @@ mod tests {
         println!("{}", bm.title);
         println!("{}", bm.artist);
         println!("{}", bm.difficulty.circle_size);
+
+        // print bm to file
+        std::fs::write("./test.osu_dec", format!("{:#?}", bm));
     }
 }
